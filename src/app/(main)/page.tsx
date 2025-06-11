b8fd7fd4a8608ec9cf6102ca9e67e8b0f7854334
todaryooo <todaryooo@gmail.com>
2025-06-12 08:55:20 +0900

    feat imageupload

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,4 @@
-import ImageSelector from "@/features/imageUpload/ImageSelector";
+import ImageSelector from "@/features/image-upload/components/ImageSelector";
 
 // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
--- src/features/image-upload/components/ImageSelector.tsx
+++ src/features/image-upload/components/ImageSelector.tsx
@@ -1,44 +1,24 @@
 "use client";
 
 import MarkerBlock from "@/features/image-upload/components/MarkerBlock";
-import { Marker } from "@/features/image-upload/types/marker";
-import { markModeAtom } from "@/lib/jotai/atom";
 import clsx from "clsx";
-import { useAtomValue } from "jotai";
-import {
-  ChangeEvent,
-  FC,
-  MouseEvent,
-  useCallback,
-  useRef,
-  useState,
-} from "react";
+import { ChangeEvent, FC, useCallback, useRef, useState } from "react";
 
 interface ImageSelectorProps {
   width?: string;
   height?: string;
 }
 
-// interface MarkerBlockProps {
-//   id: number;
-//   x: string;
-//   y: string;
-// }
-
 const ImageSelector: FC<ImageSelectorProps> = ({
   width = "w-fit",
   height = "h-fit",
 }) => {
   const [image, setImage] = useState<string | null>(null);
-  const [markers, setMarkers] = useState<Marker[]>([]);
-  const isMarkMode = useAtomValue(markModeAtom);
   const inputRef = useRef<HTMLInputElement>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
 
   const handleClick = useCallback(() => {
-    if (image) return;
     inputRef.current?.click();
-  }, [image]);
+  }, []);
 
   const handleFileChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -53,22 +33,6 @@ const ImageSelector: FC<ImageSelectorProps> = ({
     }
   }, []);
 
-  const handleClickAddMarker = useCallback(
-    (e: MouseEvent) => {
-      if (!containerRef.current || !isMarkMode) return;
-
-      const rect = containerRef.current.getBoundingClientRect();
-
-      const [clX, clY] = [e.clientX, e.clientY];
-      const [x, y] = [clX - rect.left, clY - rect.top];
-
-      const newMarker: Marker = { id: markers.length + 1, x, y };
-
-      setMarkers((prev) => [...prev, newMarker]);
-    },
-    [markers, isMarkMode],
-  );
-
   return (
     <div
       className={clsx(
@@ -79,36 +43,19 @@ const ImageSelector: FC<ImageSelectorProps> = ({
       )}
       onClick={handleClick}
     >
-      {markers.length > 0 &&
-        markers.map(({ id, x, y }) => (
-          <MarkerBlock key={id} id={id} x={x} y={y} />
-        ))}
+      {/* {markers.length > 0 && */}
+      {/* markers.map(({ id, x, y }) => ( */}
+      {/* <MarkerBlock key={id} id={id} x={x} y={y} /> */}
+      {/* ))} */}
 
-      {image ? (
-        <div
-          ref={containerRef}
-          className="w-full h-full"
-          onClick={handleClickAddMarker}
-        >
-          <img
-            src={image}
-            alt="selected image"
-            draggable={false}
-            className="w-full h-full object-contain select-none"
-          />
-        </div>
-      ) : (
-        <>
-          <p>クリックして画像を選択</p>
-          <input
-            className="hidden"
-            type="file"
-            ref={inputRef}
-            accept="image/*"
-            onChange={handleFileChange}
-          />
-        </>
-      )}
+      <p>クリックして画像を選択</p>
+      <input
+        className="hidden"
+        type="file"
+        ref={inputRef}
+        accept="image/*"
+        onChange={handleFileChange}
+      />
     </div>
   );
 };
