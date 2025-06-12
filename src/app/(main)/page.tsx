82ba1b200c2000c9406d04e08ea78203ddf0b061
todaryooo <todaryooo@gmail.com>
2025-06-13 00:01:13 +0900

    fix:prettier

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client'
 
-import { CustomTable } from "@/components/composite/CustomTable";
-import { Separator } from "@/components/ui/separator";
-import { Description } from "@/features/description/types/description";
-import ImageAnnotatorContainer from "@/features/image-upload/components/ImageAnnotationContainer";
+import { CustomTable } from '@/components/composite/CustomTable'
+import { Separator } from '@/components/ui/separator'
+import { Description } from '@/features/description/types/description'
+import ImageAnnotatorContainer from '@/features/image-upload/components/ImageAnnotationContainer'
 
 // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
@@ -11,23 +11,23 @@ export default function Home() {
   const arr: Description[] = [
     {
       id: 1,
-      name: "qwerty",
-      type: "qwerty",
-      displayData: "qwerty",
-      displayEvent: "qwerty",
-      note: "qwerty",
+      name: 'qwerty',
+      type: 'qwerty',
+      displayData: 'qwerty',
+      displayEvent: 'qwerty',
+      note: 'qwerty',
     },
     {
       id: 2,
-      name: "asdfgh",
-      type: "asdfgh",
-      displayData: "asdfgh",
-      displayEvent: "asdfgh",
-      note: "asdfgh",
+      name: 'asdfgh',
+      type: 'asdfgh',
+      displayData: 'asdfgh',
+      displayEvent: 'asdfgh',
+      note: 'asdfgh',
     },
-  ];
+  ]
 
-  const header = ["項目ID", "項目名", "型", "表示データ", "イベント", "備考"];
+  const header = ['項目ID', '項目名', '型', '表示データ', 'イベント', '備考']
 
   return (
     <div className="flex flex-col gap-4 w-full">
@@ -38,5 +38,5 @@ export default function Home() {
       </div>
       {/* <DescriptionTable dataArr={arr} /> */}
     </div>
-  );
+  )
 }
